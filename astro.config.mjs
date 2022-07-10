84302f9b09943dd1ed4e092c941299f8e9e5c47b
Aleksander Brymora <aleksander.australia@gmail.com>
2022-07-10 10:42:05 +0200

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  site: "https://aleksanderbrymora.github.io/kb",
-  base: "/astro-kb",
+  adapter: netlify(),
 });
